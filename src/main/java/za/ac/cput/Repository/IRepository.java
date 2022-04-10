6bb6657cab256eca9b4fa3f1e3e734909391e6f8
duncanharris12 <61689105+duncanharris12@users.noreply.github.com>
2022-04-10 16:32:36 +0200

    Update IRepository.java

--- src/main/java/za/ac/cput/Repository/IRepository.java
+++ src/main/java/za/ac/cput/Repository/IRepository.java
@@ -1,9 +1,8 @@
 package za.ac.cput.Repository;
 
-public interface IRepository<T, ID>
-{
-   T create(T t);
-   T read(ID id);
-   T update(T t);
-   boolean delete(ID id);
-}
+public interface IRepository <obj,Attr>{
+    obj create(obj Obj1);
+    obj read(Attr ID);
+    obj update(obj Obj2);
+    boolean delete(Attr ID);
+    }
